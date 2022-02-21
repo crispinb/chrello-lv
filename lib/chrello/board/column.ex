4d95b0f6613019a5af9f8f5be10622308b816d56
Crispin Bennett <myself@crisbennett.com>
2022-02-21 15:27:05 +1100

    Rearrange modules

--- lib/chrello/board/column.ex
+++
@@ -1,9 +0,0 @@
-defmodule Chrello.Board.Column do
-  @enforce_keys [:id, :name]
-  defstruct [:id, :name, :cards]
-  @type t :: %__MODULE__{id: integer(), name: String.t(), cards: list(Chrello.Board.Card.t())}
-
-  def new(%{"id" => id, "name" => name}) do
-    %__MODULE__{id: id, name: name}
-  end
-end
