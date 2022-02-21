d0ab7704613e866502b66a2b42780608652eee99
Crispin Bennett <myself@crisbennett.com>
2022-02-21 15:25:16 +1100

    Add test data

---
+++ test/test_data/test_data.ex
@@ -0,0 +1,19 @@
+defmodule Chrello.TestData.Load do
+  @moduledoc false
+
+  def list do
+    File.read!("test/test_data/list.json")
+  end
+
+  def tasks do
+    File.read!("test/test_data/tasks.json")
+  end
+
+  def user do
+    File.read!("test/test_data/user.json")
+  end
+
+  def user_bad_token do
+    File.read!("test/test_data/user_bad_token.json")
+  end
+end
